e37b6ef693992c6571504ef9ca88e1da691077c0
pavel <pavels.grjozs@gmail.com>
2018-08-13 14:38:19 +0300

    #1 Задача 1 выполнена #2 Задача 2 - сложности. Понятно как находить одинаковые эллементы а вот как их удалять. Если удалить сразу то у строки, у которой эллемент был удален, поменяются индексы и соотвественно поиск уже минует 1 значение. Убегает вперед.

--- Lesson_2.py
+++ Lesson_2.py
@@ -24,14 +24,19 @@ for fruits in fruits_list:
 
 print('')
 
+
 # Задача-2:
 # Даны два произвольные списка.
 # Удалите из первого списка элементы, присутствующие во втором списке.
 
-print('Задача 2. Уровень: Легко.')
+# print('Задача 2. Уровень: Легко.')
 
 list_1 = ["Tom", "Sam", "Bob", "Bill", "Alice", "Sam", "Bob", "Don Carleone", "Tom"]
 list_2 = ["Tom", "Sam", "Bob"]
+remove_list = []
+i = 0
+
+print(list_1)
 
 variable_of_list_1 = 0
 variable_of_list_2 = 0
@@ -39,24 +44,26 @@ variable_of_list_2 = 0
 len_of_list_1 = len(list_1)
 len_of_list_2 = len(list_2)
 
-while variable_of_list_2 < len(list_2):
-    while variable_of_list_1 < len(list_1):
+
+while variable_of_list_2 < len_of_list_2:
+
+    while variable_of_list_1 < len_of_list_1:
+
         if list_1[variable_of_list_1] == list_2[variable_of_list_2]:
-            print(list_1[variable_of_list_1])
+            print(variable_of_list_1)
+
+        #list_1.pop(variable_of_list_1)
         variable_of_list_1 += 1
-    else:
-     variable_of_list_2 += 1
 
-print(list_1)
-print(list_2)
+    variable_of_list_2 += 1
+    variable_of_list_1 = 0
 
-
-
-
-print('')
 
 
 # Задача-3:
 # Дан произвольный список из целых чисел.
 # Получите НОВЫЙ список из элементов исходного, выполнив следующие условия:
 # если элемент кратен двум, то разделить его на 4, если не кратен, то умножить на два.
+
+
+
