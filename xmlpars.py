e54b5c55d337318565b0a66623e34e25b451b820
wraithking <pavels.grjozs@edu.rtu.lv>
2018-09-24 15:20:47 +0300

    config
    исправления

--- xmlpars.py
+++ xmlpars.py
@@ -1,12 +1,23 @@
+#подключаем библиотеку
 import xml.etree.ElementTree as ET
+
+#загружае файл 
 tree = ET.parse('config.xml')
+
+#читаем файл в переменную
 root = tree.getroot()
 
+#задаем корень - надо ли?
 root.findall(".")
 print(root.tag)
-#
-# root.findall(".//*[@name='DEMOAPP']/configuration-property")
 
-a = root.findall("./security-configuration/*[@name='DEMOAPP']")
+#найти и вывести всех детей корня
+for child in root:
+    print(child.tag, child.attrib)
 
-print(a)
+#найти и вывести конкретный эллемент
+for appdata in root.findall("app-deployment/*[@name='Adapter']"):
+    name = appdata.get('name')
+    target = appdata.get('target')
+    plan = appdata.get('plan')
+    print(name, target, plan)
