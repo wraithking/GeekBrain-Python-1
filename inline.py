adba9dfb5742f4f1687a87e64698aa827a61142c
wraithking <pavels.grjozs@edu.rtu.lv>
2018-09-25 14:33:02 +0300

    inline
    test

---
+++ inline.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import fileinput
+
+old_plan = '/u01/app/oracle/product/Plan.xml'
+new_plan = '/u01/app/oracle/product/Adapter_plan.xml'
+
+#
+# print(new_plan)
+# print(old_plan)
+
+with fileinput.FileInput('config.xml', inplace=True) as file:
+    # for each line in file operation
+    for line in file:
+        # If we not find specific XML file
+        if old_plan and new_plan not in line:
+            # Not find string - add new string
+            print(
+                line.replace('<name>DbAdapter</name>', '<name>DbAdapter</name>\n<plan-path>', new_plan, '</plan-path>'),
+                end='')
+
+        else:
+            # Change xml file
+            print(line.replace(old_plan, new_plan), end='')
