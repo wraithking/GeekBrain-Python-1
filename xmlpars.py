674d4088a65058c991d2f8f3adced48a164b17e0
wraithking <pavels.grjozs@edu.rtu.lv>
2018-09-25 12:46:29 +0300

    Решение
    Решение но при выводе Python
    1. Режет теги Было - <plan-dir xsi:nil="true"></plan-dir>  Стало - <plan-dir xsi:nil="true" />
    2. Нету определения документа в начале. <?xml version='1.0' encoding='UTF-8'?>

--- xmlpars.py
+++ xmlpars.py
@@ -1,23 +1,30 @@
 #подключаем библиотеку
 import xml.etree.ElementTree as ET
 
-#загружае файл 
-tree = ET.parse('config.xml')
 
-#читаем файл в переменную
+ET.register_namespace("", "http://xmlns.oracle.com/weblogic/domain")
+ET.register_namespace("sec", "http://xmlns.oracle.com/weblogic/security")
+ET.register_namespace("xsi", "http://www.w3.org/2001/XMLSchema-instance")
+ET.register_namespace("wls", "http://xmlns.oracle.com/weblogic/security/wls")
+ET.register_namespace("pas","http://xmlns.oracle.com/weblogic/security/providers/passwordvalidator")
+ET.register_namespace("schemaLocation", "http://xmlns.oracle.com/weblogic/security/wls http://xmlns.oracle.com/weblogic/security/wls/1.0/wls.xsd http://xmlns.oracle.com/weblogic/domain http://xmlns.oracle.com/weblogic/1.0/domain.xsd http://xmlns.oracle.com/weblogic/security/xacml http://xmlns.oracle.com/weblogic/security/xacml/1.0/xacml.xsd http://xmlns.oracle.com/weblogic/security/providers/passwordvalidator/1.0/passwordvalidator.xsd http://xmlns.oracle.com/weblogic/security http://xmlns.oracle.com/weblogic/1.0/security.xsd")
+
+
+#загружаем файл
+tree = ET.parse('config_3.xml')
+
+#Получаем дерево эллементов (или объект типа дерево ? )
 root = tree.getroot()
 
-#задаем корень - надо ли?
+
+#Задаем корень - надо ли?
 root.findall(".")
-print(root.tag)
 
-#найти и вывести всех детей корня
-for child in root:
-    print(child.tag, child.attrib)
+#нНйти и заменить конкретный эллемент
+for appdata in root.findall(
+        "{http://xmlns.oracle.com/weblogic/domain}app-deployment[{http://xmlns.oracle.com/weblogic/domain}name='Adapter']"):
+    appdata.find(
+        '{http://xmlns.oracle.com/weblogic/domain}plan-path').text = "/u01/app/oracle/product/Adapter_plan_1024.xml"
 
-#найти и вывести конкретный эллемент
-for appdata in root.findall("app-deployment/*[@name='Adapter']"):
-    name = appdata.get('name')
-    target = appdata.get('target')
-    plan = appdata.get('plan')
-    print(name, target, plan)
+    #записать в файл
+    tree.write('config_3.xml')
