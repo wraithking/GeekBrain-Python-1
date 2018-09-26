d82d2804df14e27fb5e8c3a20f122e152f4d9bc4
wraithking <pavels.grjozs@edu.rtu.lv>
2018-09-26 12:31:57 +0300

    Update inline_work.py

--- inline_work.py
+++ inline_work.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 import fileinput
 
+old_plan = '/u01/app/oracle/product/Adapter.rar'
+new_plan = '/u01/app/oracle/product/New_Adapter.rar'
+
+old_tag = '<name>Adapter</name>'
+new_tag = "<name>Adapter</name> <plan-path>" + new_plan + "</plan-path>"
+
 with fileinput.FileInput('config.xml', inplace=True) as file:
     for line in file:
-        print(line.replace('/u01/app/oracle/product/Adapter.rar', '/u01/app/oracle/product/New_Adapter.rar'), end='')
+        print(line.replace(old_tag, new_tag), end='')
+fileinput.close()
